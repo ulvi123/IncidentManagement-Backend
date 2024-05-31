b00dd59deeeb6bb96f667942d9089a86e57cf5f4
Ulvi Nasibli <ulvi@vriendly.co>
2024-05-31 11:16:40 +0300

    chnages in .env-added database creds

--- src/config.py
+++ src/config.py
@@ -5,6 +5,14 @@ class Settings(BaseSettings):
     NGROK_AUTHTOKEN: str
     SLACK_BOT_TOKEN: str
     SLACK_VERIFICATION_TOKEN: str
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name : str
+    database_username : str
+    secret_key : str
+    algorithm : str
+    access_token_expire_minutes : int
     
     class Config():
         env_file = ".env"
---
+++ src/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine # type: ignore
+from sqlalchemy.ext.declarative import declarative_base #type: ignore
+from sqlalchemy.orm import sessionmaker
+from .config import settings
+
+
+SQLALCHEMY_DATABASE_URL =  f"postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
